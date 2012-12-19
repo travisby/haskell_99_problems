660377977c42265ed97a8fa6320db62ed84fcccd
Travis Beatty <travisby@gmail.com>
2012-12-19 00:13:38 -0500

    Linted all of our current code.  Future could should be linted before commiting

--- 01/01.hs
+++ 01/01.hs
@@ -1,3 +1,2 @@
 --  Find the last element of a list.
-myLast :: [x] -> x
-myLast x = last x
+myLast = last
--- 06/06.hs
+++ 06/06.hs
@@ -1,3 +1,3 @@
 -- Find out whether a list is a palindrome. A palindrome can be read forward or backward; e.g. (x a m a x).
 isPalindrome :: (Eq x) => [x] -> Bool
-isPalindrome xs = (xs == reverse xs)
+isPalindrome xs = xs == reverse xs
